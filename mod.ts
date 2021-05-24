a499f984890fb8a689a6cf72311b46cd78004063
Sol Boucher <sboucher@cmu.edu>
2021-05-24 16:48:27 -0400

    mod.ts: Deno Deploy "frontend" for serving real HTTP 302 redirects
    This is necessary because Google Apps Script does not permit custom headers.

---
+++ mod.ts
@@ -0,0 +1,28 @@
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+// Copyright (C) 2021, Sol Boucher <sol@vanguardcampaign.org>
+// Copyright (C) 2021, The Vanguard Campaign Corps Mods (vanguardcampaign.org)
+
+async function handler(request: Request) {
+	const redir = await fetch(BACKEND + '?link=' + request.url + '&plaintext=true');
+	return Response.redirect(await redir.text(), 302);
+}
+
+const BACKEND = Deno.env.get('BACKEND');
+if(BACKEND)
+	addEventListener('fetch', function(event: any) {
+		event.respondWith(handler(event.request));
+	});
+else
+	console.log('Environment is missing $BACKEND');
