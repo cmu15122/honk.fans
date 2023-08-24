3fc17c8a78263b508609751f429c63012cbe80e7
Pranav Addepalli <pranav.addepalli@gmail.com>
2023-08-24 12:54:47 -0400

    changed log to show URL being requested

--- mod.ts
+++ mod.ts
@@ -17,7 +17,7 @@
 async function handler(request: Request) {
 	const url = new URL(request.url);
 	const redir = await fetch(BACKEND + '?link=' + url.pathname + '&plaintext=true');
-	console.log(redir);
+	console.log(BACKEND + '?link=' + url.pathname + '&plaintext=true');
 	console.log(redir.text());
 	return Response.redirect(await redir.text(), 302);
 }
